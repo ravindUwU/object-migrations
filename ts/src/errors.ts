9c1e3cf80f425c7b6d7c737e6dbdf1a6d9f4cfdd
Ravindu Liyanapathirana <Ravindu.Liyanapathirana@monash.edu>
2024-12-28 17:02:07 +1100

    [ts] Add doc comments

--- ts/src/errors.ts
+++ ts/src/errors.ts
@@ -1,5 +1,8 @@
 import { type Version } from './migrator.js';
 
+/**
+ * Base class for all errors thrown by the migrator.
+ */
 export abstract class MigratorError extends Error {
 	constructor(name: string, message: string) {
 		super(message);
@@ -7,19 +10,46 @@ export abstract class MigratorError extends Error {
 	}
 }
 
+/**
+ * Wraps an error thrown by the migration function, while migrating an object between successive
+ * versions.
+ */
 export class MigrationError extends MigratorError {
 	constructor(
+		/**
+		 * Version from which migration was attempted. This would be the current version of the
+		 * object.
+		 */
 		public readonly from: Version,
+
+		/**
+		 * Version to which migration was attempted.
+		 */
 		public readonly to: Version,
+
+		/**
+		 * The underlying (wrapped) error.
+		 */
 		public readonly cause: unknown,
 	) {
 		super('MigrationError', `Error thrown while migrating an object from version ${from} to ${to}`);
 	}
 }
 
+/**
+ * Indicates that the sequence of steps to migrate an object between two versions couldn't be
+ * determined.
+ */
 export class NoMigrationStepsError extends MigratorError {
 	constructor(
+		/**
+		 * Version from which migration was attempted.
+		 */
 		public readonly from: Version,
+
+		/**
+		 * Version to which migration was attempted.
+		 */
 		public readonly to: Version,
 	) {
 		super('NoMigrationStepsError', `No migration steps from version ${from} to ${to}`);
--- ts/src/migrator.ts
+++ ts/src/migrator.ts
@@ -1,4 +1,4 @@
-import { MigrationError, NoMigrationStepsError } from './errors.js';
+import { MigrationError, NoMigrationStepsError, type MigratorError } from './errors.js';
 
 export type Version = string | number;
 
@@ -10,14 +10,28 @@ export interface Step {
 	readonly migration: Migration<unknown, unknown>;
 }
 
+/**
+ * Result of a successful migration.
+ */
 export interface Migrated<TTo> {
+	/**
+	 * The migrated object.
+	 */
 	readonly value: TTo;
+
+	/**
+	 * Whether the object changed during the migration. `false` if the object was already at the
+	 * version that it was attempted to be migrated to.
+	 */
 	readonly changed: boolean;
 }
 
 export class Migrator {
 	readonly prevStep = new Map<Version, Step>();
 
+	/**
+	 * Registers a migration between two successive versions.
+	 */
 	register<TFrom, TTo>(from: Version, to: Version, migration: Migration<TFrom, TTo>): void {
 		this.prevStep.set(to, {
 			from,
@@ -26,7 +40,19 @@ export class Migrator {
 		});
 	}
 
+	/**
+	 * Migrates an object between two versions.
+	 *
+	 * @param obj The object to migrate.
+	 * @param from The version of the object.
+	 * @param to The version to which the object is migrated.
+	 *
+	 * @remarks
+	 * May throw the following {@link MigratorError}s in the process: {@link NoMigrationStepsError},
+	 * {@link MigrationError}.
+	 */
 	migrate<TTo>(obj: unknown, from: Version, to: Version): Migrated<TTo> {
+		// Same versions? Return immediately.
 		if (from === to) {
 			return {
 				value: obj as TTo,
@@ -34,6 +60,10 @@ export class Migrator {
 			};
 		}
 
+		// Assemble steps.
+		// Repeatedly append the step from the previous version, starting at version `to`, until
+		// (and including) the step whose previous version is `from`. Assuming versions 1 through 4
+		// and an object being migrated from 1 to 4, the assembled steps would be [3to4, 2to3, 1to2].
 		let steps = this.tryGetCachedSteps(from, to);
 		if (steps === undefined) {
 			steps = [];
@@ -58,6 +88,8 @@ export class Migrator {
 			this.cacheSteps(from, to, steps);
 		}
 
+		// Migrate.
+		// Reverse-iterate steps, apply each function.
 		let migratedObj = obj;
 		for (let i = steps.length - 1; i >= 0; i--) {
 			try {
@@ -75,10 +107,17 @@ export class Migrator {
 
 	readonly cache = new Map</* from */ Version, Map</* to */ Version, Step[]>>();
 
+	/**
+	 * Gets the steps previously cached via {@link cacheSteps}, to migrate an object between two
+	 * versions, or `undefined` if no steps have been cached yet.
+	 */
 	tryGetCachedSteps(from: Version, to: Version): Step[] | undefined {
 		return this.cache.get(from)?.get(to);
 	}
 
+	/**
+	 * Caches steps to migrate an object between two versions.
+	 */
 	cacheSteps(from: Version, to: Version, steps: Step[]): void {
 		if (steps.length === 0) {
 			throw new RangeError('There must be at least 1 step to be cached.');
