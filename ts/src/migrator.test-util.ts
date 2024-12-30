5412b80e6c10a0ade58f66d64c06f76e275f9143
Ravindu Liyanapathirana <Ravindu.Liyanapathirana@monash.edu>
2024-12-30 21:38:58 +1100

    [ts] Add class object counterparts to all existing plain object migration tests

--- ts/src/migrator.test-util.ts
+++ ts/src/migrator.test-util.ts
@@ -1,4 +1,32 @@
 import { Migrator } from './migrator.js';
+import { type Class } from './util.js';
+
+// Classes
+
+export abstract class TestClass {
+	sequence: number[];
+
+	constructor(readonly version: number) {
+		this.sequence = [version];
+	}
+}
+
+// prettier-ignore
+export class TestClass1 extends TestClass { constructor() { super(1); } }
+
+// prettier-ignore
+export class TestClass2 extends TestClass { constructor() { super(2); } }
+
+// prettier-ignore
+export class TestClass3 extends TestClass { constructor() { super(3); } }
+
+// prettier-ignore
+export class TestClass4 extends TestClass { constructor() { super(4); } }
+
+// prettier-ignore
+export class TestClass5 extends TestClass { constructor() { super(5); } }
+
+// Plain objects
 
 export interface TestObj<V extends number> {
 	readonly version: V;
@@ -12,10 +40,41 @@ export function makeTestObj<V extends number>(v: V): TestObj<V> {
 	};
 }
 
-export function makeTestObjMigrator(): Migrator {
+// Migrator
+
+export function makeTestMigrator(): Migrator {
 	const m = new Migrator();
 
-	function register(fromVersion: number, toVersion: number): void {
+	// Classes
+
+	function registerTestClass<TFrom extends TestClass, TTo extends TestClass>(
+		fromClass: Class<TFrom>,
+		toClass: Class<TTo>,
+	): void {
+		m.register<TFrom, TTo>(
+			fromClass,
+			toClass,
+			(o) => {
+				const c = new toClass();
+				c.sequence = [...o.sequence, c.version];
+				return c;
+			},
+			(o) => {
+				const c = new fromClass();
+				c.sequence = [...o.sequence, c.version];
+				return c;
+			},
+		);
+	}
+
+	registerTestClass(TestClass1, TestClass2);
+	registerTestClass(TestClass2, TestClass3);
+	registerTestClass(TestClass3, TestClass4);
+	registerTestClass(TestClass4, TestClass5);
+
+	// Plain objects
+
+	function registerTestObj(fromVersion: number, toVersion: number): void {
 		m.register<TestObj<number>, TestObj<number>>(
 			fromVersion,
 			toVersion,
@@ -34,10 +93,10 @@ export function makeTestObjMigrator(): Migrator {
 		);
 	}
 
-	register(1, 2);
-	register(2, 3);
-	register(3, 4);
-	register(4, 5);
+	registerTestObj(1, 2);
+	registerTestObj(2, 3);
+	registerTestObj(3, 4);
+	registerTestObj(4, 5);
 
 	return m;
 }
--- ts/src/migrator.test.ts
+++ ts/src/migrator.test.ts
@@ -2,148 +2,316 @@ import { test, suite, type TestContext } from 'node:test';
 import { Migrator } from './migrator.js';
 import { MigrationError, NoMigrationStepsError } from './errors.js';
 import { TestError, type Mocked } from './test-util.js';
-import { makeTestObj, makeTestObjMigrator, type TestObj } from './migrator.test-util.js';
+import {
+	makeTestObj,
+	makeTestMigrator,
+	type TestObj,
+	TestClass3,
+	TestClass1,
+	TestClass5,
+	TestClass2,
+	TestClass4,
+} from './migrator.test-util.js';
 
 suite('Migrator', () => {
 	suite('Returns the same object when from & to are the same', () => {
-		const m = makeTestObjMigrator();
-		const o = makeTestObj(1);
+		const m = makeTestMigrator();
 
-		test('Forward', (t: TestContext) => {
-			const res = m.forward<TestObj<3>>(o, 3, 3);
+		suite('Classes', () => {
+			const o = new TestClass3();
 
-			t.assert.strictEqual(res.changed, false);
-			t.assert.strictEqual(res.value, o);
+			test('Forward', (t: TestContext) => {
+				const res = m.forward(o, TestClass3);
+
+				t.assert.strictEqual(res.changed, false);
+				t.assert.strictEqual(res.value, o);
+			});
+
+			test('Backward', (t: TestContext) => {
+				const res = m.backward(o, TestClass3);
+
+				t.assert.strictEqual(res.changed, false);
+				t.assert.strictEqual(res.value, o);
+			});
 		});
 
-		test('Backward', (t: TestContext) => {
-			const res = m.backward<TestObj<3>>(o, 3, 3);
+		suite('Plain objects', () => {
+			const o = makeTestObj(3);
 
-			t.assert.strictEqual(res.changed, false);
-			t.assert.strictEqual(res.value, o);
+			test('Forward', (t: TestContext) => {
+				const res = m.forward<TestObj<3>>(o, 3, 3);
+
+				t.assert.strictEqual(res.changed, false);
+				t.assert.strictEqual(res.value, o);
+			});
+
+			test('Backward', (t: TestContext) => {
+				const res = m.backward<TestObj<3>>(o, 3, 3);
+
+				t.assert.strictEqual(res.changed, false);
+				t.assert.strictEqual(res.value, o);
+			});
 		});
 	});
 
 	suite('Runs steps in order', () => {
-		const m = makeTestObjMigrator();
+		const m = makeTestMigrator();
 
-		test('Forward 1 to 5', (t: TestContext) => {
-			const o = makeTestObj(1);
-			const res = m.forward<TestObj<5>>(o, 1, 5);
+		suite('Classes', () => {
+			test('Forward 1 to 5', (t: TestContext) => {
+				const o = new TestClass1();
+				const res = m.forward(o, TestClass5);
 
-			t.assert.strictEqual(res.changed, true);
-			t.assert.strictEqual(res.value.version, 5);
-			t.assert.deepStrictEqual(res.value.sequence, [1, 2, 3, 4, 5]);
+				t.assert.strictEqual(res.changed, true);
+				t.assert.ok(res.value instanceof TestClass5);
+				t.assert.deepStrictEqual(res.value.sequence, [1, 2, 3, 4, 5]);
+			});
+
+			test('Forward 2 to 4', (t: TestContext) => {
+				const o = new TestClass2();
+				const res = m.forward(o, TestClass4);
+
+				t.assert.strictEqual(res.changed, true);
+				t.assert.ok(res.value instanceof TestClass4);
+				t.assert.deepStrictEqual(res.value.sequence, [2, 3, 4]);
+			});
+
+			test('Backward 5 to 1', (t: TestContext) => {
+				const o = new TestClass5();
+				const res = m.backward(o, TestClass1);
+
+				t.assert.strictEqual(res.changed, true);
+				t.assert.ok(res.value instanceof TestClass1);
+				t.assert.deepStrictEqual(res.value.sequence, [5, 4, 3, 2, 1]);
+			});
+
+			test('Backward 4 to 2', (t: TestContext) => {
+				const o = new TestClass4();
+				const res = m.backward(o, TestClass2);
+
+				t.assert.strictEqual(res.changed, true);
+				t.assert.ok(res.value instanceof TestClass2);
+				t.assert.deepStrictEqual(res.value.sequence, [4, 3, 2]);
+			});
 		});
 
-		test('Forward 2 to 4', (t: TestContext) => {
-			const o = makeTestObj(2);
-			const res = m.forward<TestObj<4>>(o, 2, 4);
+		suite('Plain objects', () => {
+			test('Forward 1 to 5', (t: TestContext) => {
+				const o = makeTestObj(1);
+				const res = m.forward<TestObj<5>>(o, 1, 5);
 
-			t.assert.strictEqual(res.changed, true);
-			t.assert.strictEqual(res.value.version, 4);
-			t.assert.deepStrictEqual(res.value.sequence, [2, 3, 4]);
-		});
+				t.assert.strictEqual(res.changed, true);
+				t.assert.strictEqual(res.value.version, 5);
+				t.assert.deepStrictEqual(res.value.sequence, [1, 2, 3, 4, 5]);
+			});
 
-		test('Backward 5 to 1', (t: TestContext) => {
-			const o = makeTestObj(5);
-			const res = m.backward<TestObj<1>>(o, 5, 1);
+			test('Forward 2 to 4', (t: TestContext) => {
+				const o = makeTestObj(2);
+				const res = m.forward<TestObj<4>>(o, 2, 4);
 
-			t.assert.strictEqual(res.changed, true);
-			t.assert.strictEqual(res.value.version, 1);
-			t.assert.deepStrictEqual(res.value.sequence, [5, 4, 3, 2, 1]);
-		});
+				t.assert.strictEqual(res.changed, true);
+				t.assert.strictEqual(res.value.version, 4);
+				t.assert.deepStrictEqual(res.value.sequence, [2, 3, 4]);
+			});
 
-		test('Backward 4 to 2', (t: TestContext) => {
-			const o = makeTestObj(4);
-			const res = m.backward<TestObj<2>>(o, 4, 2);
+			test('Backward 5 to 1', (t: TestContext) => {
+				const o = makeTestObj(5);
+				const res = m.backward<TestObj<1>>(o, 5, 1);
 
-			t.assert.strictEqual(res.changed, true);
-			t.assert.strictEqual(res.value.version, 2);
-			t.assert.deepStrictEqual(res.value.sequence, [4, 3, 2]);
+				t.assert.strictEqual(res.changed, true);
+				t.assert.strictEqual(res.value.version, 1);
+				t.assert.deepStrictEqual(res.value.sequence, [5, 4, 3, 2, 1]);
+			});
+
+			test('Backward 4 to 2', (t: TestContext) => {
+				const o = makeTestObj(4);
+				const res = m.backward<TestObj<2>>(o, 4, 2);
+
+				t.assert.strictEqual(res.changed, true);
+				t.assert.strictEqual(res.value.version, 2);
+				t.assert.deepStrictEqual(res.value.sequence, [4, 3, 2]);
+			});
 		});
 	});
 
 	suite('Throws when there are no steps between from & to', () => {
-		const m = makeTestObjMigrator();
+		const m = makeTestMigrator();
 
-		test('Forward', (t: TestContext) => {
-			t.assert.throws(() => {
-				m.forward(makeTestObj(1), 1, -1);
-			}, NoMigrationStepsError);
+		suite('Classes', () => {
+			// eslint-disable-next-line @typescript-eslint/no-extraneous-class -- Intentional.
+			class TestClass0 {}
+
+			test('Forward', (t: TestContext) => {
+				t.assert.throws(() => {
+					m.forward(new TestClass1(), TestClass0);
+				}, NoMigrationStepsError);
+			});
+
+			test('Backward', (t: TestContext) => {
+				t.assert.throws(() => {
+					m.backward(new TestClass1(), TestClass0);
+				}, NoMigrationStepsError);
+			});
 		});
 
-		test('Backward', (t: TestContext) => {
-			t.assert.throws(() => {
-				m.backward(makeTestObj(1), 1, -1);
-			}, NoMigrationStepsError);
+		suite('Plain objects', () => {
+			test('Forward', (t: TestContext) => {
+				t.assert.throws(() => {
+					m.forward(makeTestObj(1), 1, -1);
+				}, NoMigrationStepsError);
+			});
+
+			test('Backward', (t: TestContext) => {
+				t.assert.throws(() => {
+					m.backward(makeTestObj(1), 1, -1);
+				}, NoMigrationStepsError);
+			});
 		});
 	});
 
 	suite('Wraps errors thrown during migrations', () => {
 		const m = new Migrator();
 
+		m.register(
+			TestClass1,
+			TestClass2,
+			() => {
+				throw new TestError('class/forward');
+			},
+			() => {
+				throw new TestError('class/backward');
+			},
+		);
+
 		m.register<TestObj<1>, TestObj<2>>(
 			1,
 			2,
 			() => {
-				throw new TestError('forward');
+				throw new TestError('plain/forward');
 			},
 			() => {
-				throw new TestError('backward');
+				throw new TestError('plain/backward');
 			},
 		);
 
-		test('Forward', (t: TestContext) => {
-			t.assert.throws(
-				() => {
-					m.forward(makeTestObj(1), 1, 2);
-				},
-				(e) =>
-					e instanceof MigrationError &&
-					e.cause instanceof TestError &&
-					e.cause.payload === 'forward',
-			);
+		suite('Classes', () => {
+			test('Forward', (t: TestContext) => {
+				t.assert.throws(
+					() => {
+						m.forward(new TestClass1(), TestClass2);
+					},
+					(e) =>
+						e instanceof MigrationError &&
+						e.cause instanceof TestError &&
+						e.cause.payload === 'class/forward',
+				);
+			});
+
+			test('Backward', (t: TestContext) => {
+				t.assert.throws(
+					() => {
+						m.backward(new TestClass2(), TestClass1);
+					},
+					(e) =>
+						e instanceof MigrationError &&
+						e.cause instanceof TestError &&
+						e.cause.payload === 'class/backward',
+				);
+			});
 		});
 
-		test('Backward', (t: TestContext) => {
-			t.assert.throws(
-				() => {
-					m.backward(makeTestObj(2), 2, 1);
-				},
-				(e) =>
-					e instanceof MigrationError &&
-					e.cause instanceof TestError &&
-					e.cause.payload === 'backward',
-			);
+		suite('Plain objects', () => {
+			test('Forward', (t: TestContext) => {
+				t.assert.throws(
+					() => {
+						m.forward(makeTestObj(1), 1, 2);
+					},
+					(e) =>
+						e instanceof MigrationError &&
+						e.cause instanceof TestError &&
+						e.cause.payload === 'plain/forward',
+				);
+			});
+
+			test('Backward', (t: TestContext) => {
+				t.assert.throws(
+					() => {
+						m.backward(makeTestObj(2), 2, 1);
+					},
+					(e) =>
+						e instanceof MigrationError &&
+						e.cause instanceof TestError &&
+						e.cause.payload === 'plain/backward',
+				);
+			});
 		});
 	});
 
 	suite('Uses cached steps in subsequent migrations', () => {
-		test('Forward', (t: TestContext) => {
-			const m = makeTestObjMigrator() as Mocked<Migrator, 'computeSteps'>;
+		function makeMockedMigrator(t: TestContext): Mocked<Migrator, 'computeSteps'> {
+			const m = makeTestMigrator();
 			t.mock.method(m, 'computeSteps');
+			return m as Mocked<Migrator, 'computeSteps'>;
+		}
 
-			m.forward(makeTestObj(1), 1, 5);
-			t.assert.ok(m.tryGetCachedSteps(1, 5) !== undefined);
-			t.assert.strictEqual(m.computeSteps.mock.callCount(), 1);
-			m.computeSteps.mock.resetCalls();
+		suite('Classes', () => {
+			test('Forward', (t: TestContext) => {
+				const m = makeMockedMigrator(t);
 
-			m.forward(makeTestObj(1), 1, 5);
-			t.assert.strictEqual(m.computeSteps.mock.callCount(), 0);
+				t.assert.ok(m.tryGetCachedSteps(TestClass1, TestClass5) === undefined);
+
+				m.forward(new TestClass1(), TestClass5);
+				t.assert.ok(m.tryGetCachedSteps(TestClass1, TestClass5) !== undefined);
+				t.assert.strictEqual(m.computeSteps.mock.callCount(), 1);
+				m.computeSteps.mock.resetCalls();
+
+				m.forward(new TestClass1(), TestClass5);
+				t.assert.strictEqual(m.computeSteps.mock.callCount(), 0);
+			});
+
+			test('Backward', (t: TestContext) => {
+				const m = makeMockedMigrator(t);
+
+				t.assert.ok(m.tryGetCachedSteps(TestClass5, TestClass1) === undefined);
+
+				m.backward(new TestClass5(), TestClass1);
+				t.assert.ok(m.tryGetCachedSteps(TestClass5, TestClass1) !== undefined);
+				t.assert.strictEqual(m.computeSteps.mock.callCount(), 1);
+				m.computeSteps.mock.resetCalls();
+
+				m.backward(new TestClass5(), TestClass1);
+				t.assert.strictEqual(m.computeSteps.mock.callCount(), 0);
+			});
 		});
 
-		test('Backward', (t: TestContext) => {
-			const m = makeTestObjMigrator() as Mocked<Migrator, 'computeSteps'>;
-			t.mock.method(m, 'computeSteps');
+		suite('Plain objects', () => {
+			test('Forward', (t: TestContext) => {
+				const m = makeMockedMigrator(t);
 
-			m.backward(makeTestObj(5), 5, 1);
-			t.assert.ok(m.tryGetCachedSteps(5, 1) !== undefined);
-			t.assert.strictEqual(m.computeSteps.mock.callCount(), 1);
-			m.computeSteps.mock.resetCalls();
+				t.assert.ok(m.tryGetCachedSteps(1, 5) === undefined);
 
-			m.backward(makeTestObj(5), 5, 1);
-			t.assert.strictEqual(m.computeSteps.mock.callCount(), 0);
+				m.forward(makeTestObj(1), 1, 5);
+				t.assert.ok(m.tryGetCachedSteps(1, 5) !== undefined);
+				t.assert.strictEqual(m.computeSteps.mock.callCount(), 1);
+				m.computeSteps.mock.resetCalls();
+
+				m.forward(makeTestObj(1), 1, 5);
+				t.assert.strictEqual(m.computeSteps.mock.callCount(), 0);
+			});
+
+			test('Backward', (t: TestContext) => {
+				const m = makeMockedMigrator(t);
+
+				t.assert.ok(m.tryGetCachedSteps(5, 1) === undefined);
+
+				m.backward(makeTestObj(5), 5, 1);
+				t.assert.ok(m.tryGetCachedSteps(5, 1) !== undefined);
+				t.assert.strictEqual(m.computeSteps.mock.callCount(), 1);
+				m.computeSteps.mock.resetCalls();
+
+				m.backward(makeTestObj(5), 5, 1);
+				t.assert.strictEqual(m.computeSteps.mock.callCount(), 0);
+			});
 		});
 	});
 });
