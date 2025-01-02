d2aa5f05ae83b09406d809058aad66f86dbd2f91
Ravindu Liyanapathirana <Ravindu.Liyanapathirana@monash.edu>
2025-01-02 19:53:52 +1100

    [ts] Add `pack` script to assemble NPM tarball

--- ts/object-migrations-import-test/scripts/ensure-build.mjs
+++ ts/object-migrations-import-test/scripts/ensure-build.mjs
@@ -1,19 +1,21 @@
+/**
+ * Builds object-migrations, if it isn't built yet.
+ */
+
 import fs from 'node:fs';
 import path from 'node:path';
 import childProcess from 'node:child_process';
 import console from 'node:console';
 
-const base = path.resolve(import.meta.dirname, '..', '..', 'object-migrations');
-
-const esmExists = fs.existsSync(path.resolve(base, 'dist', 'esm'));
-const cjsExists = fs.existsSync(path.resolve(base, 'dist', 'cjs'));
+const baseDir = path.resolve(import.meta.dirname, '..', '..', 'object-migrations');
+const esmExists = fs.existsSync(path.resolve(baseDir, 'dist', 'esm'));
+const cjsExists = fs.existsSync(path.resolve(baseDir, 'dist', 'cjs'));
 
 if (!esmExists || !cjsExists) {
-	console.log({ esmExists, cjsExists }, 'Rebuilding...');
+	console.log({ esmExists, cjsExists }, 'Building');
 
 	childProcess.execSync('pnpm run build', {
-		cwd: base,
-		encoding: 'utf8',
+		cwd: baseDir,
 		stdio: 'inherit',
 	});
 }
---
+++ ts/object-migrations/scripts/pack.mjs
@@ -0,0 +1,84 @@
+/**
+ * Packs object-migrations using `pnpm pack`, building it first if necessary, into the `pack`
+ * directory.
+ *
+ * - Include the arg `build` to force a build.
+ * - The tarball is be generated to `pack/*.tgz`.
+ * - `pack/filename` includes the basename of the tarball.
+ */
+
+import fs from 'node:fs';
+import path from 'node:path';
+import process from 'node:process';
+import console from 'node:console';
+import childProcess from 'node:child_process';
+
+const args = process.argv.slice(2);
+
+/**
+ * Resolve path segments relative to the project directory (NOT the script directory).
+ * @param  {...string} segments
+ */
+function $path(...segments) {
+	return path.resolve(import.meta.dirname, '..', ...segments);
+}
+
+// Build
+if (args.includes('build') || !fs.existsSync($path('dist'))) {
+	console.log('Building');
+	childProcess.execSync('pnpm run build', {
+		cwd: $path('.'),
+		stdio: 'inherit',
+	});
+}
+
+console.log('Assembling pack directory');
+
+// pack/
+if (fs.existsSync($path('pack'))) {
+	fs.rmSync($path('pack'), { recursive: true, force: true });
+}
+fs.mkdirSync($path('pack', 'cwd'), { recursive: true });
+
+// pack/cwd/dist/**
+fs.cpSync($path('dist'), $path('pack', 'cwd', 'dist'), { recursive: true });
+
+// pack/cwd/assets/**
+fs.mkdirSync($path('pack', 'cwd', 'assets'));
+fs.copyFileSync(
+	$path('..', '..', 'assets', 'logo.svg'),
+	$path('pack', 'cwd', 'assets', 'logo.svg'),
+);
+
+// pack/cwd/package.json
+const packageJson = JSON.parse(fs.readFileSync($path('package.json'), { encoding: 'utf8' }));
+delete packageJson.scripts;
+delete packageJson.devDependencies;
+fs.writeFileSync(
+	$path('pack', 'cwd', 'package.json'),
+	JSON.stringify(packageJson, undefined, '\t'),
+);
+
+// pack/cwd/README.md
+let readmeMd = fs
+	.readFileSync($path('README.md'), { encoding: 'utf8' })
+	// Replace <img src="..." pack-src="..."> with <img src="..."> with `pack-src` instead of `src`.
+	.replace(/src="[^"]+" pack-(?<packSrc>src="[^"]+")/g, '$<packSrc>');
+fs.writeFileSync($path('pack', 'cwd', 'README.md'), readmeMd);
+
+// pack/cwd/LICENSE
+fs.copyFileSync($path('..', '..', 'LICENSE'), $path('pack', 'cwd', 'LICENSE'));
+
+console.log('Packing');
+
+// pack/*.tgz
+const packOutputJson = childProcess.execSync('pnpm pack --json --pack-destination ..', {
+	cwd: $path('pack', 'cwd'),
+	encoding: 'utf8',
+});
+const packOutput = JSON.parse(packOutputJson);
+
+// pack/filename
+fs.writeFileSync($path('pack', 'filename'), path.basename(packOutput.filename));
+
+console.log('Done');
